a118842037b6430faff669e8b7cad187d8a182c3
giaba0 <thienhanaylai@gmail.com>
2021-10-11 23:59:05 +0700

    fix

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -18,9 +18,17 @@ module.exports = {
         if (checkroom(message)) return;
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`Không có bản nhạc nào đang phát !`)
-        const volume = parseInt(args[0])
-        if (volume < 0) volume = 0;
-        if (volume > 100) volume = 80;
+        let volume;
+        let number = parseInt(args[0]);
+        if (0 < number && number < 100) {
+            volume = parseInt(number)
+        } else if (number <= 0) {
+            volume = 0;
+            message.channel.send('Rồi nhỏ dị ai nghe :)')
+        } else if (number >= 100) {
+            volume = 100;
+            message.channel.send('Bị điếc hay gì mở to vậy ?')
+        }
         if (isNaN(volume)) return message.channel.send(`Vui lòng nhập 1 số 0-100 để chỉnh âm lượng!`)
         client.distube.setVolume(message, volume)
         message.channel.send(`Volume set to \`${volume}\``)
