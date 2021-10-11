b98a5e5c8ed907356bb128236b2073309ea607e1
giaba0 <thienhanaylai@gmail.com>
2021-10-11 12:00:54 +0700

    adD

--- commands/music/lyric.js
+++ commands/music/lyric.js
@@ -28,19 +28,12 @@ module.exports = {
                     optimizeQuery: true
                 };
                 getSong(options).then(function(song) {
-                    let thumbnail;
-
-                    if (song.albumArt === null) {
-                        thumbnail = 'https://i0.wp.com/s1.uphinh.org/2021/09/25/Untitled2e84e3ca63fb3550.th.png';
-                    } else {
-                        thumbnail = song.albumArt;
-                    }
 
                     const embed = new MessageEmbed()
                         .setColor('BLACK')
                         .setTitle(` ${args.join(' ')}`)
-                        .setThumbnail(thumbnail)
-                        .setDescription(`${song.lyrics}`)
+
+                    .setDescription(`${song.lyrics}`)
                         .setTimestamp()
                         .setFooter(`Yêu cầu bởi ${message.author.username}`, message.author.displayAvatarURL())
                     message.channel.send({ embeds: [embed] })
@@ -48,33 +41,40 @@ module.exports = {
             }
 
             if (!args.join(' ')) {
-                const queue = client.distube.getQueue(message)
-                if (!queue) return message.reply(`Không có bài nhạc nào đang phát vui lòng nhập tên bài hát !`)
-                const track = queue.songs[0];
-                const name = track.name;
-                const options = {
-                    apiKey: 'fxRWwdEIxgyNPOOldRyqbzdKVTq5bEJOCb5hu23GaLKYvaOxN0rDuQFE3swNG6pP',
-                    title: name,
-                    artist: '',
-                    optimizeQuery: true
-                };
-                getSong(options).then(function(song) {
-                    let thumbnail;
+                try {
+                    const queue = client.distube.getQueue(message)
+                    if (!queue) return message.reply(`Không có bài nhạc nào đang phát vui lòng nhập tên bài hát !`)
+                    const track = queue.songs[0];
+                    const name = track.name;
+                    const options = {
+                        apiKey: 'fxRWwdEIxgyNPOOldRyqbzdKVTq5bEJOCb5hu23GaLKYvaOxN0rDuQFE3swNG6pP',
+                        title: name,
+                        artist: '',
+                        optimizeQuery: true
+                    };
+                    getSong(options).then(function(song) {
+                        const embed = new MessageEmbed()
+                            .setColor('BLACK')
+                            .setTitle(`${name}`)
 
-                    if (song.albumArt === null) {
-                        thumbnail = 'https://i0.wp.com/s1.uphinh.org/2021/09/25/Untitled2e84e3ca63fb3550.th.png';
-                    } else {
-                        thumbnail = song.albumArt;
-                    }
-                    const embed = new MessageEmbed()
-                        .setColor('BLACK')
-                        .setTitle(`${name}`)
-                        .setThumbnail(thumbnail)
                         .setDescription(`${song.lyrics}`)
-                        .setTimestamp()
-                        .setFooter(`Yêu cầu bởi ${message.author.username}`, message.author.displayAvatarURL())
-                    message.channel.send({ embeds: [embed] })
-                })
+                            .setTimestamp()
+                            .setFooter(`Yêu cầu bởi ${message.author.username}`, message.author.displayAvatarURL())
+                        message.channel.send({ embeds: [embed] })
+                    })
+                } catch (error) {
+                    console.error(error)
+                    message.channel.send(`Not Found !`)
+                    channel1 = client.channels.cache.find(channel => channel.id === '895523356986707979')
+                    const embed = new MessageEmbed()
+                        .setColor('RED')
+                        .setTitle('Error!')
+                        .addField(`Error:`, `\`\`\`js\n${error}\n\`\`\``)
+                    channel1.send({
+                        embeds: [embed],
+                        allowedMentions: { repliedUser: false }
+                    })
+                }
             }
 
         } catch (error) {
